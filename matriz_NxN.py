6582401ed92a92f6e5fa37a1d0772d4ce0b98286
Antonio Javier García Martínez <cokesaeba@gmail.com>
2023-06-23 18:22:13 +0200

    Refactorizamos: se saca código del main y se modulariza en funciones para facilitar test

--- matriz_NxN.py
+++ matriz_NxN.py
@@ -4,22 +4,26 @@
  con números aleatorios de 1 dígito. Calcula y almacena en dos listas la suma de cada fila y columna de la matriz y las imprime.
 """
 AUTHOR = "Antonio Javier Garcia Martinez (cokesaeba@gmail.com)"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
-import logging
 import argparse
 import random
 
-def createLogger(file):
-    ''' Crea un logger con el nombre del fichero pasado como parámetro '''
 
-    LOG_FORMAT = "%(asctime)s %(levelname)s - %(message)s"
-    logging.basicConfig(
-        filename=file, level=logging.DEBUG, format=LOG_FORMAT
-    )
-    logger = logging.getLogger()
+def create_matriz(N):
+    ''' Crea una matriz NxN con números aleatorios de 1 dígito '''
+    matriz = []
 
-    return logger
+    # N > 0, no necesitamos comprobar que sea entero porque ya lo hemos forzado
+    if N < 1:
+        raise ValueError("N debe ser mayor que 0")
+
+    for i in range(N):
+        matriz.append([])
+        for j in range(N):
+            matriz[i].append(random.randint(0,9))
+
+    return matriz
 
 
 def print_matriz(matriz):
@@ -31,12 +35,27 @@ def print_matriz(matriz):
         print("|")
     print()
 
+def suma_filas(matriz):
+    ''' Suma las filas de la matriz pasada como parámetro y devuelve una lista con los resultados '''
+    sum_filas = []
+
+    for i in range(len(matriz)):
+        sum_filas.append(sum(matriz[i]))
+
+    return sum_filas
+
+def suma_columnas(matriz):
+    ''' Suma las columnas de la matriz pasada como parámetro y devuelve una lista con los resultados '''
+    sum_columnas = []
+
+    # Otra forma de hacerlo, más legible (como en el ejemplo del curso de IBM): 
+    for c in range(len(matriz)):
+        sum_columnas.append(sum([row[c] for row in matriz]))
+
+    return sum_columnas
+
 def main():
     
-    # Creamos un logger
-    logger = createLogger("matriz.log")
-    logger.info("Inicio del programa")
-
     # args:
     parser = argparse.ArgumentParser(description="Matriz NxN")
     
@@ -52,18 +71,9 @@ def main():
     #positional args... 
     N = args.N
 
-    # N > 0, no necesitamos comprobar que sea entero porque ya lo hemos forzado
-    if N < 1:
-        logger.error("N debe ser mayor que 0")
-        raise ValueError("N debe ser mayor que 0")
-    
     # Create matriz NxN
-    matriz = []
+    matriz = create_matriz(N)
 
-    for i in range(N):
-        matriz.append([])
-        for j in range(N):
-            matriz[i].append(random.randint(0,9))
 
     # Print matriz
     print(f"Matriz {N}x{N}\n")
@@ -76,24 +86,14 @@ def main():
     sum_filas = []
     sum_columnas = []
 
-    # Sumamos las filas y las columnas y las almacenamos en las listas
-    # Sumamos las filas, sum() suma todos los elementos de una lista (o iterable), cada lista de la matriz es una fila
-    for i in range(N):
-        sum_filas.append(sum(matriz[i]))
-
-    # Sumamos las columnas, sumamos los elementos de cada columna, para ello recorremos la matriz por columnas:
-    #for j in range(N):
-    #    sum_columnas.append(sum([matriz[i][j] for i in range(N)]))
-
-    # Otra forma de hacerlo, más legible (como en el ejemplo del curso de IBM): 
-    for c in range(N):
-        sum_columnas.append(sum([row[c] for row in matriz]))
+    # Sumamos las filas y columnas
+    sum_filas = suma_filas(matriz)
+    sum_columnas = suma_columnas(matriz)
+     
 
     # Imprimimos los resultados, me gusta más utilizar f-strings que format():
     print(f"\nSuma de las filas: {sum_filas}")
     print(f"\nSuma de las columnas: {sum_columnas}")
-
-    logger.info("Fin programa!")
 
 if __name__ == "__main__":
     main()
